fb4ab7f2ea0a6690499762c8bf2906fbb84ad149
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-05-05 19:56:57 +0530

    Addewd Icons

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,19 @@
+import "./Card.css";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="job-card">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo neque id
+      veritatis. Minus necessitatibus quas aliquid ullam quo maiores? Quasi
+      mollitia hic distinctio totam deserunt explicabo tenetur nam, ipsa ex
+      molestias ducimus, placeat commodi accusantium, est ratione? Architecto,
+      provident enim quo aliquid omnis nesciunt esse soluta dignissimos
+      similique, ducimus nam? Est, nam? Ab sint delectus cum rerum eum quo
+      incidunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad non
+      dolorem eos consectetur asperiores accusamus veniam voluptate saepe fuga
+      eum?
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/routes/Home/HomePage.jsx
+++ src/routes/Home/HomePage.jsx
@@ -1,9 +1,17 @@
 import Card from "../../components/card/Card";
 import "./HomePage.css";
 const HomePage = () => {
-  return <div className="home-page">
-    <Card />
-  </div>;
+  return (
+    <div className="home-page">
+      <div className="home-page-content">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
