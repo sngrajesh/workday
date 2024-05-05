6b836640e8326c71f7a0f0d556b01c8d8ea58424
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-05-06 00:32:22 +0530

    Addewd Icons

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,76 @@
 import "./Card.css";
 
-const Card = () => {
+const Card = ({
+  companyName,
+  jobRole,
+  location,
+  minExp,
+  maxExp,
+  minJdSalary,
+  maxJdSalary,
+  salaryCurrencyCode,
+  logoUrl,
+  jobDetailsFromCompany,
+  jdLink,
+}) => {
   return (
-    <div className="job-card">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo neque id
-      veritatis. Minus necessitatibus quas aliquid ullam quo maiores? Quasi
-      mollitia hic distinctio totam deserunt explicabo tenetur nam, ipsa ex
-      molestias ducimus, placeat commodi accusantium, est ratione? Architecto,
-      provident enim quo aliquid omnis nesciunt esse soluta dignissimos
-      similique, ducimus nam? Est, nam? Ab sint delectus cum rerum eum quo
-      incidunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad non
-      dolorem eos consectetur asperiores accusamus veniam voluptate saepe fuga
-      eum?
+    <div className="job-wrapper">
+      <div className="job-detail">
+        <div className="posted-date-applicants">
+          <div className="posted-date">Posted 2 days ago</div>
+          <div className="applicants">10 Applicants</div>
+        </div>
+        <div className="job-company-detail">
+          <img className="company-logo" src={logoUrl} alt="company-logo" />
+          <div className="job-company-detail-innder-wrapper">
+            <div className="company-name">{companyName}</div>
+            <div className="job-role">{jobRole}</div>
+            <div className="job-location">{location}</div>
+          </div>
+        </div>
+        <div className="job-salary">{`Estimated Salary:  ${salaryCurrencyCode}${minJdSalary} - ${maxJdSalary}LPA`}</div>
+        <div className="companydetai-jobdetail">
+          <h3>About Company: </h3>
+          <p className="job-detail-paragrahp">
+            FalconX provides comprehensive access to the deepest digital asset
+            liquidity across the globe. Through our prime brokerage platform,
+            FalconX 360, investors unlock and scale returns faster and more
+            efficiently than any other platform. Our 24/7, dedicated team
+            enables investors to navigate dynamic markets around the clock by
+            providing support for all account, operational and trading needs.
+          </p>
+          <h3>About Role: </h3>
+          <p className="job-detail-paragrahp">{jobDetailsFromCompany}</p>
+        </div>
+        <div className="bottom-overlay-content">
+          <button className="view-job-button">View Job</button>
+
+          <div
+            style={{
+              opacity: minExp ? 1 : 0,
+              userSelect: maxExp ? "auto" : "none",
+            }}
+            className="experience-required"
+          >
+            <h4>Minimum Experience</h4>
+            <span>{`${minExp} years`}</span>
+          </div>
+          <div className="apply-links">
+            <button>⚡ Easy Apply</button>
+            <button className="ask-for-referral-button">
+              <img
+                src="https://media.licdn.com/dms/image/C5103AQHVt2mE6FxV8Q/profile-displayphoto-shrink_800_800/0/1534593555331?e=1717632000&v=beta&t=4it3udzASl4VF4xaqVNLIeuov1xNZK5binLTShSqA1s"
+                alt=""
+              />
+              <img
+                src="https://media.licdn.com/dms/image/C4E03AQGYqFu0X9bi0g/profile-displayphoto-shrink_100_100/0/1639030271843?e=1720656000&v=beta&t=neogXo_o-h-10bavgc8jfRwHcGa2KRPbSupzkWoOPG0"
+                alt=""
+              />
+              &nbsp; Unlock Referral Ask
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,60 +3,6 @@ import { navbarLinks } from "../../assets/Data";
 import { Link } from "react-router-dom";
 import React from "react";
 
-/*
-
-export const navbarLinks = {
-  job: {
-    title: "GET JOBS",
-    hiddenTitle: "LOOKING FOR A JOB",
-    links: [
-      {
-        title: "My applied jobs",
-        path: "/jobs",
-        icon: UserIcon ,
-      },
-      {
-        title: "Search jobs",
-        path: "/jobs",
-        icon: SearchIcon ,
-      },
-      {
-        title: "Search salary",
-        path: "/jobs",
-        icon: CurrencyIocnINR ,
-      },
-      {
-        title: "Ask for referral",
-        path: "/jobs",
-        icon: UserPlusIcon ,
-      },
-    ],
-  },
-
-  refer: {
-    title: "REFER",
-    hiddenTitle: "RECOMMEND AND EARN",
-    links: [
-      {
-        title: "Recommend from shortlist",
-        path: "/jobs",
-        icon: ThumbsUpIcon ,
-      },
-      {
-        title: "Recommend to specific company",
-        path: "/jobs",
-        icon: TaskIcon ,
-      },
-      {
-        title: "Refer this extension",
-        path: "/jobs",
-        icon: ShareIcon ,
-      },
-    ],
-  },
-};
-
-*/
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -75,7 +21,9 @@ const Sidebar = () => {
               <ul>
                 {navbarLinks[key].links.map((link, index) => {
                   return (
-                    <li key={index}>
+                    <li key={index}  
+                      data-tooltip={link.title}
+                    >
                       <Link to={link.path}>
                         {React.createElement(link.icon, {
                           className: "sidebar-icon",
